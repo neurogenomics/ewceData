ad7112fdd4f553d2b3e1b8bb5d8bd2f514e65059
Al-Murphy <alanmurph94@hotmail.com>
2021-03-23 08:08:38 +0000

    Updating load method in zzz

--- R/zzz.R
+++ R/zzz.R
@@ -1,37 +1,38 @@
 #' @import ExperimentHub
 #' @importFrom AnnotationHub query
+#' @importFrom AnnotationHub mcols
 #' @importFrom utils read.csv
 #.onLoad <- function(libname, pkgname) {
-#    fl <- system.file("extdata", "metadata.csv", package=pkgname)
-#    titles <- read.csv(fl, stringsAsFactors=FALSE)$Title
-#    ExperimentHub::createHubAccessors(pkgname, titles)
+#  objs <- read.csv(system.file("extdata", "metadata.csv",
+#                               package="ewceData"),
+#                   stringsAsFactors=FALSE,fileEncoding = "UTF-8-BOM")$Title
+#  if (!length(objs))
+#    stop("no objects found")
+
+#  ## Functions to load objects by name:
+#  ns <- asNamespace(pkgname)
+#  eh <- query(ExperimentHub(), "ewceData")
+#  sapply(objs,
+#         function(xx) {
+#           func = function(metadata = FALSE) {
+#             if (!isNamespaceLoaded("ExperimentHub"))
+#               attachNamespace("ExperimentHub")
+#             #grepl exact match, avoid issues with all_mgi and all_mgi_wtEnsembl
+#             ehid <- rownames(mcols(eh)[mcols(eh)$title==xx,])
+#             #ehid <- names(query(eh, paste0("\\b",xx,"\\b")))
+#             if (!length(ehid))
+#               stop(paste0("resource ", xx,
+#                           " not found in ExperimentHub"))
+#             if (metadata)
+#               eh[ehid]
+#             else eh[[ehid]]
+#           }
+#           assign(xx, func, envir=ns)
+#           namespaceExport(ns, xx)
+#         })
 #}
 .onLoad <- function(libname, pkgname) {
-  objs <- read.csv(system.file("extdata", "metadata.csv",
-                               package="ewceData"),
-                   stringsAsFactors=FALSE,fileEncoding = "UTF-8-BOM")$Title
-  if (!length(objs))
-    stop("no objects found")
-
-  ## Functions to load objects by name:
-  ns <- asNamespace(pkgname)
-  sapply(objs,
-         function(xx) {
-           func = function(metadata = FALSE) {
-             if (!isNamespaceLoaded("ExperimentHub"))
-               attachNamespace("ExperimentHub")
-             eh <- query(ExperimentHub(), "ewceData")
-             #ehid <- names(query(eh, xx))
-             #grepl exact match, avoid issues with all_mgi and all_mgi_wtEnsembl
-             ehid <- names(query(eh, paste0("\\b",xx,"\\b")))
-             if (!length(ehid))
-               stop(paste0("resource ", xx,
-                           " not found in ExperimentHub"))
-             if (metadata)
-               eh[ehid]
-             else eh[[ehid]]
-           }
-           assign(xx, func, envir=ns)
-           namespaceExport(ns, xx)
-         })
+  fl <- system.file("extdata", "metadata.csv", package=pkgname)
+  titles <- read.csv(fl, stringsAsFactors=FALSE)$Title
+  createHubAccessors(pkgname, titles)
 }
